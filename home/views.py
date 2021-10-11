364ed6aa7a69daf7fed84b4b74b144ef0f0ac7b0
gzdlmn <gzdlmn01@gmail.com>
2021-10-11 19:04:56 +0300

    edited html

--- home/views.py
+++ home/views.py
@@ -8,5 +8,4 @@ def home_page(request):
     today = date.today()
     pi = math.pi
     e = math.e
-    current_machine_id = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-    return render(request, "home.html", {"today":today, "pi":pi, "e":e, "current_machine_id":current_machine_id})
+    return render(request, "home.html", {"today":today, "pi":pi, "e":e})
